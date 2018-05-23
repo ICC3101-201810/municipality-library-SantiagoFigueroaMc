56530199ebd65a868ec5c2310f923eb52f74fed0
Santiago Figueroa <37884952+SantiagoFigueroaMc@users.noreply.github.com>
2018-05-22 21:13:34 -0400

    Entrega lab 10

---
+++ vle_content_file.file.ac8e7f95fabbab78.5265666c656374696f6e322e7a6970/Reflection2/Persona.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Reflection2
+{
+    public class Persona
+    {
+       
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public int Age
+        {
+            get;
+            set;
+        }
+
+        public int Height
+        {
+            get;
+            set;
+        }
+
+        public int Weight
+        {
+            get;
+            set;
+        }
+
+
+        public string SayHello()
+        {
+            return "Hello!";
+        }
+
+        public string Speak(string wordsToSpeak)
+        {
+            return wordsToSpeak;
+        }
+    }
+}
---
+++ vle_content_file.file.ac8e7f95fabbab78.5265666c656374696f6e322e7a6970/Reflection2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+
+namespace Reflection2
+{
+    //Fuente: https://johnlnelson.com/tag/type-getinterfaces/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Assembly.LoadFile(@"C:\Users\sfigu\Desktop\vle_content_file.file.ac8e7f95fabbab78.5265666c656374696f6e322e7a6970\Reflection2\bin\Debug\netcoreapp2.0\ClassLibrary1.dll");
+            Type[] types = a.GetTypes();
+            foreach (Type type in types)
+            {
+                if (!type.IsPublic)
+                {
+                    continue;
+                }
+
+                MemberInfo[] members = type.GetMembers(BindingFlags.Public
+                                                      | BindingFlags.Instance
+                                                      | BindingFlags.InvokeMethod);
+                foreach (MemberInfo member in members)
+                {
+                    Console.WriteLine(member.MemberType + "." + type.Name + "." + member.Name + "." + member.MetadataToken);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
